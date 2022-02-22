7a6fd26f623465d7d3ca895c77e017ea89194067
thanhptt1 <phamthuatieuthanh@gmail.com>
2022-02-22 18:11:29 +0700

    update loss

--- yolo/yolo_v1/loss/loss.py
+++ yolo/yolo_v1/loss/loss.py
@@ -1,31 +1,24 @@
+from utils import IOU
 import torch
+import torch.nn as nn
 
-class Loss:
-    def __init__(self, ) -> None:
-        pass
+class Loss(nn.Module):
+    def __init__(self, S=7, B=2, C=3, lambda_noobj=0.5, lambda_coord=5):
+        super().__init__()
+        self.mse = nn.MSELoss(reduce='sum')
+        self.S = S
+        self.B = B
+        self.C = C
+        self.lambda_noobj = lambda_noobj
+        self.lambda_coord = lambda_coord
 
+    def forward(self, preds, target):
 
+        """
+            preds: [c1, c2, ..., cn, x1, y1, x2, y2, conf1, x3, y3, x4, y4, conf2]
+        """
+        iou_b1 = IOU(preds[..., self.C + 1: self.C + 5], target[..., self.C + 1:self.C + 5])
+        iou_b2 = IOU(preds[..., self.C + 6: self.C + 10], target[..., self.C + 6:self.C + 10])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+        iou_maxes, bestbox = torch.max(ious, dims=0)
 
-
-    # def IOU(self, boxA, boxB):
-    #     inter_x = max(0, min(boxA[0] + boxA[2] * 0.5, boxB[0] + boxB[2] * 0.5) - 
-    #                         max(boxA[0] - boxA[2] * 0.5, boxB[0] - boxB[2] * 0.5))
-
-    #     inter_y = max(0, min(boxA[1] + boxA[3] * 0.5, boxB[1] + boxB[3] * 0.5) - 
-    #                         max(boxA[1] - boxA[3] * 0.5, boxB[1] - boxB[3] * 0.5))
-        
-    #     inter = inter_x * inter_y
-    #     union = boxA[2] * boxA[3] + boxB[2] * boxB[3] - inter
-        
-    #     return inter / union
-    
-    def IOU(self, boxA, boxB):
-        inter_x = torch.min(boxA[:, :, :, 0] + boxA[:, :, :, 2] * 0.5, boxB[:, :, :, 0] + boxB[:, :, :, 2] * 0.5) - \
-                  torch.max(boxA[:, :, :, 0] - boxA[:, :, :, 2] * 0.5, boxB[:, :, :, 0] - boxB[:, :, :, 2] * 0.5)
-        inter_y = torch.min(boxA[:, :, :, 1] + boxA[:, :, :, 3] * 0.5, boxB[:, :, :, 1] + boxB[:, :, :, 3] * 0.5) - \
-                  torch.max(boxA[:, :, :, 1] - boxA[:, :, :, 3] * 0.5, boxB[:, :, :, 1] - boxB[:, :, :, 3] * 0.5)
-        inter = torch.max(0, inter_x) * torch.max(0, inter_y)
-        union = boxA[:, :, :, 2] * boxA[:, :, :, 3] + boxB[:, :, :, 2] * boxB[:, :, :, 3] - inter
-        return inter / union
-    
-     
